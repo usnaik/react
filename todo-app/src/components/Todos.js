336764813744c5638f35a8e2f5130876c5aab31f
usnaik <upendra_naik@yahoo.com>
2019-12-30 12:50:14 +0530

    Final changes

--- todo-app/src/components/Todos.js
+++ todo-app/src/components/Todos.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import '../App.css';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
-    render() {
-        console.log(this.props.todos)
-        return this.props.todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo}/>
-        ));
-    }
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+    ));
+  }
 }
 
-
+// PropTypes
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
 }
 
 export default Todos;
